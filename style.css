7727b5428dd5968b108de16ef7f9f8bc3ec5b30b
mandrusanjay <71212016+mandrusanjay@users.noreply.github.com>
2020-09-16 14:39:46 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,98 @@
+<style>
+ *{box-sizing: border-box; margin: 0; padding: 0;
+ }
+ h1{text-align:
+center;color:yellow;margin-bottom: 10px;background:url("jk.png") no-repeat top center; height: 180px;
+ }
+ #contentbox,#contents{background-color: lightblue;
+border:solid black; width:33.3333%;
+float:left;margin: 5px; overflow:auto;
+ }	
+#chicken{background-color: orange;
+	width:30%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:69.5%;
+}
+#beef{background-color: pink;
+	width:16%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:83.5%;
+}
+#sushi{background-color: purple;
+	width:18%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:81.5%;
+}
+@media(min-width: 992px){
+#contentbox,#contents{background-color: lightblue;
+border:solid black; width:31%;
+float:left;margin: 5px;overflow:auto;
+ }	
+#chicken{background-color: orange;
+	width:30%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:69%;
+}
+#beef{background-color: pink;
+	width:16%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:83%;
+}
+#sushi{background-color: purple;
+	width:18%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:81%;
+}
+	
+}
+
+@media(min-width: 768px) and (max-width: 991px){
+#contentbox{background-color: lightblue;
+border:solid black; width:48%;
+margin: 5px;float:left;overflow:auto;
+ }		
+#contents{width:98%;
+background-color: lightblue;
+border:solid black;
+margin: 5px;float:left;}
+#chicken{background-color: orange;
+	width:40%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:59.5%;
+}
+#beef{background-color: pink;
+	width:20%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:79.5%;
+}
+#sushi{background-color: purple;
+	width:20%;
+	text-align: center;
+	border:solid black;
+	position: relative;
+	left:79.5%;	
+}	
+}
+@media(max-width: 767px){
+#contentbox,#contents{background-color: lightblue;
+border:solid black; width:100%;
+margin: 5px;overflow:auto;
+ }
+}	
+</style>
